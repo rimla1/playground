b28a82dac1c6d32613edc188d72e58cf99bd3158
Almir Muminovic <projectalmir@gmail.com>
2022-03-13 12:54:04 +0100

    env example to to protect your important code

---
+++ EnvVariables/index.js
@@ -0,0 +1,10 @@
+const dotEnv = require('dotenv')
+dotEnv.config()
+
+const express = require('express')
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
