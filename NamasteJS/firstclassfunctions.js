80d4204302bb964ee326d3e1590dc8504f0d2441
Almir Muminovic <projectalmir@gmail.com>
2022-03-16 23:12:09 +0100

    Function Statement

---
+++ NamasteJS/firstclassfunctions.js
@@ -0,0 +1,18 @@
+// Function Statement
+function a() {
+    console.log('a called')
+}
+
+// Function Expression
+
+// Function Declaration
+
+// Anonymous Function
+
+// Named Function Expression
+
+// Difference between Parameters & Arguments
+
+// First Class Functions
+
+// Arrow Functions
