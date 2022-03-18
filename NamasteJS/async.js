015608b7b726f3230430e61c72f228413f184c65
Almir Muminovic <projectalmir@gmail.com>
2022-03-18 23:17:31 +0100

    How Event Listeners work in jS

--- NamasteJS/async.js
+++ NamasteJS/async.js
@@ -1,10 +1,19 @@
 // Callback Queue and Event Loop 
-
 console.log('Start')
 
 setTimeout(() => {
-    console.log('callback')
+    console.log('Callback')
 }, 5000)
+
+console.log('End')
+
+
+// How Event Listeners Work in JS
+console.log('Start')
+
+document.getElementById('btn').addEventListener('click', () => {
+    console.log('Callback')
+})
 
 console.log('End')
 
