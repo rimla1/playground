71c21d7820431c63742fb7d70ea3324e1f6230bb
Almir Muminovic <projectalmir@gmail.com>
2022-03-21 19:34:43 +0100

    Reduce() example

--- NamasteJS/mapfilterreduce.js
+++ NamasteJS/mapfilterreduce.js
@@ -2,7 +2,7 @@
 
 // Example 1: double, triple, binary
 
-const arr = [5, 1, 3, 2, 6]
+const arrMap = [5, 1, 3, 2, 6]
 
 const double = (x) => {
     return x * 2
@@ -16,12 +16,57 @@ const binary = (x) => {
     return x.toString(2);
 }
 
-const output = arr.map(binary)
-console.log(output)
+const outputMap = arrMap.map(binary)
+console.log(outputMap)
 
 // We can pass function too
 
-const output1 = arr.map((x) =>  x * 3)
+const outputMap1 = arrMap.map((x) =>  x * 3)
 
-console.log(output1)
+console.log(outputMap1)
 
+// Filter
+
+// Example 1: Filter odd values
+
+const arrFilter = [2, 4, 5, 6, 7, 9]
+
+const greaterThanFour = (y) => {
+    return x > 4
+}
+
+const isOdd = (y) => {
+    return y % 2
+}
+
+const isEven = (y) => {
+    return y % 2 === 0;
+}
+
+const outputFilter = arrFilter.filter(isEven)
+console.log(outputFilter)
+
+// Reduce
+
+// Example 1: sum or max
+
+const arrReduce = [120, 130, 140, 150, 160]
+
+const findSum = (arr) => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+    }
+    return sum
+}
+
+console.log(findSum(arrReduce))
+
+const findSum = (acc, curr) => {
+    acc = acc + curr
+    return acc
+}
+
+const output = arrReduce.reduce(findSum, 0)
+
+console.log(output)
