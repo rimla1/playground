725ddd29100e361c73d2d051951d472eee34a43b
Almir Muminovic <projectalmir@gmail.com>
2022-03-21 23:14:18 +0100

    Example with reduce()

--- NamasteJS/mapfilterreduce.js
+++ NamasteJS/mapfilterreduce.js
@@ -1,72 +1,84 @@
-//Map
+// //Map
 
-// Example 1: double, triple, binary
+// // Example 1: double, triple, binary
 
-const arrMap = [5, 1, 3, 2, 6]
+// const arrMap = [5, 1, 3, 2, 6]
 
-const double = (x) => {
-    return x * 2
-}
+// const double = (x) => {
+//     return x * 2
+// }
 
-const triple = (x) => {
-    return x * 3
-}
+// const triple = (x) => {
+//     return x * 3
+// }
 
-const binary = (x) => {
-    return x.toString(2);
-}
+// const binary = (x) => {
+//     return x.toString(2);
+// }
 
-const outputMap = arrMap.map(binary)
-console.log(outputMap)
+// const outputMap = arrMap.map(binary)
+// console.log(outputMap)
 
-// We can pass function too
+// // We can pass function too
 
-const outputMap1 = arrMap.map((x) =>  x * 3)
+// const outputMap1 = arrMap.map((x) =>  x * 3)
 
-console.log(outputMap1)
+// console.log(outputMap1)
 
-// Filter
+// // Filter
 
-// Example 1: Filter odd values
+// // Example 1: Filter odd values
 
-const arrFilter = [2, 4, 5, 6, 7, 9]
+// const arrFilter = [2, 4, 5, 6, 7, 9]
 
-const greaterThanFour = (y) => {
-    return x > 4
-}
+// const greaterThanFour = (y) => {
+//     return x > 4
+// }
 
-const isOdd = (y) => {
-    return y % 2
-}
+// const isOdd = (y) => {
+//     return y % 2
+// }
 
-const isEven = (y) => {
-    return y % 2 === 0;
-}
+// const isEven = (y) => {
+//     return y % 2 === 0;
+// }
 
-const outputFilter = arrFilter.filter(isEven)
-console.log(outputFilter)
+// const outputFilter = arrFilter.filter(isEven)
+// console.log(outputFilter)
 
-// Reduce
+// // Reduce
 
-// Example 1: sum or max
+// // Example 1: sum 
 
-const arrReduce = [120, 130, 140, 150, 160]
+// const arrReduce = [120, 130, 140, 150, 160]
 
-const findSum = (arr) => {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i]
+// const findSum = (arr) => {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         sum = sum + arr[i]
+//     }
+//     return sum
+// }
+
+// console.log(findSum(arrReduce))
+
+// const findSum = (acc, curr) => {
+//     acc = acc + curr
+//     return acc
+// }
+
+// const output = arrReduce.reduce(findSum, 0)
+
+// console.log(output)
+
+// // Example 1: max value of array 
+const arrMax = [10, 25, 50, 100, 1001, 54]
+
+const output = arrMax.reduce((max, curr) => {
+    if (curr > max) {
+        max = curr
     }
-    return sum
-}
-
-console.log(findSum(arrReduce))
-
-const findSum = (acc, curr) => {
-    acc = acc + curr
-    return acc
-}
-
-const output = arrReduce.reduce(findSum, 0)
+    return max
+}, 0)
 
 console.log(output)
