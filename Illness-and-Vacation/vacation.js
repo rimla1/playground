90a614baddf719cb7d72f69af9ba0a575ca096c7
Almir Muminovic <projectalmir@gmail.com>
2022-04-26 20:45:19 +0200

    update

--- Illness-and-Vacation/vacation.js
+++ Illness-and-Vacation/vacation.js
@@ -2,7 +2,7 @@ const isProgrammerOnVacation = false;
 
 const stateOfProgrammer = (state) => {
   if (!state) {
-    console.log("Stomach problem 25.04.2022");
+    console.log("Stomach problem 26.04.2022");
   } else {
     console.log("work!");
   }
