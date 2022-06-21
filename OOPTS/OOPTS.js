001ccbe56ce1cad50aa8de0dc7bf19bd69c0ffa0
Almir Muminovic <projectalmir@gmail.com>
2022-06-21 17:16:03 +0200

    OOP typescript introduction

---
+++ OOPTS/OOPTS.js
@@ -0,0 +1,12 @@
+"use strict";
+class Player {
+    // health: number
+    // speed: number
+    greet() {
+        console.log("Hello world!");
+    }
+}
+const mario = new Player();
+// mario.health = 100
+// mario.speed = 1
+mario.greet();
