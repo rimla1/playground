ef311590d9d0fdfd28fede36d8c2b6eba1aede20
Almir Muminovic <projectalmir@gmail.com>
2022-06-21 17:52:53 +0200

    Understanding Objects

--- OOPTS/OOPTS.ts
+++ OOPTS/OOPTS.ts
@@ -1,20 +1,20 @@
-class Player {
-    // Instance variables
-    // health: number
-    // speed: number
+// class Player {
+//     // Instance variables
+//     // health: number
+//     // speed: number
 
-    // Methods
-    greet() {
-        console.log("Hello world!")
-    }
-}
+//     // Methods
+//     greet() {
+//         console.log("Hello world!")
+//     }
+// }
 
-// Instances of the class
-const mario = new Player()
-// keyword "new" create an instance of the blueprint player
-// "mario" is new object that we created using blueprints from instance of the class
+// // Instances of the class
+// const mario = new Player()
+// // keyword "new" create an instance of the blueprint player
+// // "mario" is new object that we created using blueprints from instance of the class
 
 
-// mario.health = 100
-// mario.speed = 1
-mario.greet()
+// // mario.health = 100
+// // mario.speed = 1
+// mario.greet()
