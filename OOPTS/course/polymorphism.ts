fe95590b7bdf37ecdf8762955d1bc8e0b1c577ac
Almir Muminovic <projectalmir@gmail.com>
2022-06-25 23:31:10 +0200

    Avoid code duplication using superclass

---
+++ OOPTS/course/polymorphism.ts
@@ -0,0 +1,18 @@
+class Character{
+    
+}
+
+class Mario {
+    move() {}
+    playVoiceRecording() {}
+}
+
+class Luigi {
+    move() {}
+    playVoiceRecording() {}
+}
+
+class Peach {
+    move() {}
+    playVoiceRecording() {}
+}
