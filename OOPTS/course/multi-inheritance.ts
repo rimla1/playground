6a047bb0c3865a7cf98d2c413a15e4ee5a1b28d1
Almir Muminovic <projectalmir@gmail.com>
2022-06-30 16:10:50 +0200

    multiple inheritance, using interfaces fully abstracted classes

---
+++ OOPTS/course/multi-inheritance.ts
@@ -0,0 +1,25 @@
+abstract class LeagueChampion {
+    hunger: number;
+    health: number;
+
+    abstract eat(): void
+}
+
+interface Yorlde extends LeagueChampion {
+    yordleId: number
+}
+
+interface Adcarry extends LeagueChampion {
+    adcId: number
+}
+
+class Tristana implements Yorlde, Adcarry {
+    hunger: number;
+    health: number
+    yordleId: number;
+    adcId: number;
+
+    eat(){
+       this.hunger -= 1
+    }
+}
