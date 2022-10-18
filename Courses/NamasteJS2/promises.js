b6a4c4b974a9b4d5a10b1a013445c3c1a0edd6dc
Almir Muminovic <projectalmir@gmail.com>
2022-10-18 14:54:32 +0200

    solved

--- Courses/NamasteJS2/promises.js
+++ Courses/NamasteJS2/promises.js
@@ -1,19 +1,22 @@
-const fetch = require("node-fetch");
+const cart = ["t-shirt", "shoes", "pants"];
 
-// const cart = ["t-shirt", "shoes", "pants"];
+// Callbacks approach
+createOrderCB(cart, (orderId) => {
+  proceedToPaymentCB(orderId, (paymentInfo) => {
+    orderSummary(paymentInfo, () => {
+      updateWallet();
+    });
+  });
+}); // result[orderId]
 
-// // Callbacks approach
-// createOrderCB(cart, (orderId) => {
-//     proceedToPaymentCB(orderId)
-// }) // result[orderId]
-
-// // Promises approach
-// createOrderPromise(cart).then((orderId) => {
-//     proceedToPaymentPromise(orderId)
-// })
-
-const GITHUB_API = "https://api.github.com/users/rimla1";
-
-const user = fetch(GITHUB_API);
-
-console.log(user);
+// Promises approach
+createOrderPromise(cart)
+  .then((orderId) => {
+    proceedToPaymentPromise(orderId);
+  })
+  .then((paymentInfo) => {
+    orderSummary(paymentInfo);
+  })
+  .then(() => {
+    updateWallet();
+  });
